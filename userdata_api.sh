8b7b29ad0eb302caf55b8f18d7d7fd9c0d2fd1b6
Jeremy Kuah <jeremy.kuah.91@gmail.com>
2022-03-13 22:49:32 +0800

    Update user data for API
    Change context path to "/api" instead of "/" for use with path based routing

--- userdata_api.sh
+++ userdata_api.sh
@@ -4,4 +4,4 @@ sudo yum install java -y
 
 sudo wget -L https://github.com/JeremyKuah/SmartBankAPI_JarRepo/raw/main/smart-bank-api.jar
 
-sudo java -Dserver.port=80 -Dspring.datasource.url=jdbc:postgresql://${db_endpoint}/${db_name} -Dmailman.endpoint=http://${mailman_endpoint} -jar smart-bank-api.jar &
+sudo java -Dserver.port=80 -Dspring.datasource.url=jdbc:postgresql://${db_endpoint}/${db_name} -Dmailman.endpoint=http://${mailman_endpoint} -Dserver.servlet.context-path=/api -jar smart-bank-api.jar &
